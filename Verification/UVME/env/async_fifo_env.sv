2383f5c673ef706bb440fe1e0cea042bc9962396
Renuka-Pooja-KG <renuka.pooja@pravegasemi.com>
2025-07-26 14:04:41 +0530

    scoreboard name change in env file

--- Verification/UVME/env/async_fifo_env.sv
+++ Verification/UVME/env/async_fifo_env.sv
@@ -1,9 +1,9 @@
 class async_fifo_env extends uvm_env;
   `uvm_component_utils(async_fifo_env)
 
-  async_fifo_agent      m_fifo_agent;
-  async_fifo_coverage   m_fifo_coverage;
-  scoreboard            m_scoreboard;
+  async_fifo_agent        m_fifo_agent;
+  async_fifo_coverage     m_fifo_coverage;
+  async_fifo_scoreboard   m_scoreboard;
 
   function new(string name = "async_fifo_env", uvm_component parent = null);
     super.new(name, parent);
