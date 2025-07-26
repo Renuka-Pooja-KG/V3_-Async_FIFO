ccf19c7a6f72133685f6210c1a79c866e06aa456
Renuka-Pooja-KG <renuka.pooja@pravegasemi.com>
2025-07-26 14:32:12 +0530

    test files added to compile_list.f

--- Verification/SIM/compile_list.f
+++ Verification/SIM/compile_list.f
@@ -7,6 +7,15 @@
 
 /home/sgeuser100/renuka_dv/V3_-Async_FIFO/Verification/UVME/top/async_fifo_interface.sv
 /home/sgeuser100/renuka_dv/V3_-Async_FIFO/Verification/UVME/package/verification_pkg.sv
+
+# Test files - must be compiled after package
+/home/sgeuser100/renuka_dv/V3_-Async_FIFO/Verification/UVME/test/async_fifo_base_test.sv
+/home/sgeuser100/renuka_dv/V3_-Async_FIFO/Verification/UVME/test/async_fifo_reset_test.sv
+/home/sgeuser100/renuka_dv/V3_-Async_FIFO/Verification/UVME/test/async_fifo_write_test.sv
+/home/sgeuser100/renuka_dv/V3_-Async_FIFO/Verification/UVME/test/async_fifo_read_test.sv
+/home/sgeuser100/renuka_dv/V3_-Async_FIFO/Verification/UVME/test/async_fifo_simultaneous_test.sv
+/home/sgeuser100/renuka_dv/V3_-Async_FIFO/Verification/UVME/test/async_fifo_random_test.sv
+
 /home/sgeuser100/renuka_dv/V3_-Async_FIFO/Verification/UVME/top/async_fifo_top_tb.sv
 
 
